e68a5ecfbfc291085633a2695a6eadc53b291b8f
vihanga02 <muthumalavdw.22@uom.lk>
2024-01-19 21:04:53 +0530

    choice box done

--- src/main/java/org/example/myproject/Scene2Controller.java
+++ src/main/java/org/example/myproject/Scene2Controller.java
@@ -1,36 +1,49 @@
 package org.example.myproject;
 
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 import java.net.URL;
-import java.nio.file.attribute.AttributeView;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class Scene2Controller implements Initializable {
     private String[] categories = {"All", "Electronics", "Clothings"};
+
     @FXML
     private ChoiceBox<String> myChoiceBox;
 
     List<Product> productList;
     WestministerShoppingManager manager;
+    List<Product> newProductList = new ArrayList<>();
+    ObservableList<Product> observableProductList;
+
     public Scene2Controller() {
         this.manager = new WestministerShoppingManager();
         this.productList = manager.loadFile();
+        this.observableProductList = FXCollections.observableArrayList(productList);
     }
+
     @Override
-    public void initialize(URL arg0, ResourceBundle arg1) {
+    public void initialize(URL url, ResourceBundle rb) {
         myChoiceBox.getItems().addAll(categories);
         myChoiceBox.setOnAction(this::getCategory);
     }
-
-
-    List<Product> newProductList = new ArrayList<>();
-
     public void getCategory(ActionEvent event){
         String category = myChoiceBox.getValue();
         if (category.equals("Electronics")){
@@ -53,5 +66,14 @@ public class Scene2Controller implements Initializable {
             newProductList = productList;
             System.out.println("all");
         }
+        observableProductList.setAll(newProductList);
     }
+
+    public void switchingToScene3(ActionEvent event) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Gui2.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
 }
---
+++ src/main/java/org/example/myproject/Scene3Controller.java
@@ -0,0 +1,5 @@
+package org.example.myproject;
+
+public class Scene3Controller {
+
+}
